98e84a9a55f3ae0c31c47809826162e9cc77d12d
gkk345 <73120774+kennykos@users.noreply.github.com>
2025-12-14 10:58:45 -0600

    formatting

--- parkipy/ewald/__init__.py
+++ parkipy/ewald/__init__.py
@@ -6,13 +6,13 @@ Spectral Ewald Summation
 
 Overview
 --------
-The Ewald summation module :class:`parkipy.ewald` uses a Kokkos backend 
-to provide fast summation APIs for the combined 
+The Ewald summation module :class:`parkipy.ewald` uses a Kokkos backend
+to provide fast summation APIs for the combined
 Stokes single and double-layer [1]_ and Laplace [2]_ kernels. The Ewald
 method is extended to free directions with ideas from [3]_.
 
 Automatic parameter selection is facilitated via the :class:`EwaldOptions`
-class, where the user selects computational parameters and has the ability 
+class, where the user selects computational parameters and has the ability
 to select different Ewald methods and threading configurations.
 
 Ewald kernel are easy to call, e.g., if you are looking to compute the combined Stokes
@@ -33,7 +33,7 @@ single and double-layer potential in a fully periodic box on a GPU, you can call
 >>> options = parkipy.ewald.EwaldOptions(box=[1,1,1], periodicity=3, tolerance=1e-5, execution_space="Cuda")
 >>> potential = parkipy.ewald.stokes_comb(trg, src, dns, nrm, options)
 
-Of course, detailed documentation of the Ewald kernels and options are given below. 
+Of course, detailed documentation of the Ewald kernels and options are given below.
 
 Ewald Kernel Gallery
 ____________________
@@ -42,7 +42,7 @@ ____________________
    :toctree: generated/
 
    stokes_comb
-   laplace  
+   laplace
 
 
 Ewald Kernel Support
@@ -57,28 +57,27 @@ Ewald Kernel Support
 References
 __________
 
-.. [1] Bagge, J., & Tornberg, A.-K. (2023). 
-        Fast Ewald summation for Stokes flow with arbitrary periodicity. 
-        Journal of Computational Physics, 493, 112473. 
+.. [1] Bagge, J., & Tornberg, A.-K. (2023).
+        Fast Ewald summation for Stokes flow with arbitrary periodicity.
+        Journal of Computational Physics, 493, 112473.
         https://doi.org/10.1016/j.jcp.2023.112473
-.. [2] Shamshirgar, D. S., Bagge, J., & Tornberg, A.-K. (2021). 
-        Fast Ewald summation for electrostatic potentials with arbitrary periodicity. 
-        The Journal of Chemical Physics, 154(16), 164109. 
+.. [2] Shamshirgar, D. S., Bagge, J., & Tornberg, A.-K. (2021).
+        Fast Ewald summation for electrostatic potentials with arbitrary periodicity.
+        The Journal of Chemical Physics, 154(16), 164109.
         https://doi.org/10.1063/5.0044895
-.. [3] Vico, F., Greengard, L., & Ferrando, M. (2016). 
-    Fast convolution with free-space Green’s functions. 
-    Journal of Computational Physics, 323, 191–203. 
+.. [3] Vico, F., Greengard, L., & Ferrando, M. (2016).
+    Fast convolution with free-space Green’s functions.
+    Journal of Computational Physics, 323, 191–203.
     https://doi.org/10.1016/j.jcp.2016.07.028
 """
 
 from ._kernels import (
     EwaldOptions,
+    stokes_sl,
     stokes_comb,
     laplace,
 )
 
-from ._params import (
-        SEParams
-        )
+from ._params import SEParams
 
 __all__ = ["stokes_comb", "laplace"]
