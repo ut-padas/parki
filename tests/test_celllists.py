62a13a370c6ba38490f0b14ca369319225569bbc
Gabriel Kosmacher <73120774+kennykos@users.noreply.github.com>
2025-12-22 14:07:18 -0600

    Loop over 27 neighboring cells

--- tests/test_celllists.py
+++ tests/test_celllists.py
@@ -32,23 +32,40 @@ def cell_list(x, y, cutoff, box):
                 continue
 
             # loop over y-neighbors
-            for neighbor in range(y_list.num_nonempty_cells):
-                if neighbor == -1:
+            _area = y_list.cell_grid_shape[1] * y_list.cell_grid_shape[2]
+            cell_x = cell // _area
+            cell_y = (cell % _area) // y_list.cell_grid_shape[2]
+            cell_z = (cell % _area)  % y_list.cell_grid_shape[2]
+            for dx in range(-1,2):
+                neighbor_x = cell_x+dx
+                if neighbor_x < 0 or neighbor_x >= y_list.cell_grid_shape[0]:
                     continue
-                off_y = neighbor * y_list.cell_size
+                for dy in range(-1,2):
+                    neighbor_y = cell_y + dy
+                    if neighbor_y < 0 or neighbor_y >= y_list.cell_grid_shape[1]:
+                        continue
+                    for dz in range(-1,2):
+                        neighbor_z = cell_z + dz
+                        if neighbor_z < 0 or neighbor_z >= y_list.cell_grid_shape[2]:
+                            continue
+                        neighbor_empty = neighbor_x * _area + neighbor_y * y_list.cell_grid_shape[2] + neighbor_z
 
-                # loop over y-particles within a neighbor
-                for jj in range(off_y, off_y + y_list.cell_size):
-                    j = y_list.particle_index[jj]
-                    if j == -1:
-                        continue
-                    dist_sq = np.sum((x[:, i] - y[:, j]) ** 2)
-                    if dist_sq == 0:
-                        continue
-                    dist = np.sqrt(dist_sq)
-                    if dist < cutoff:
-                        u[i] += 1 / dist
-                        print(neighbor)
+                        neighbor = y_list.nonempty_cell_index[neighbor_empty]
+                        if neighbor == -1:
+                            continue
+                        off_y = neighbor * y_list.cell_size
+
+                        # loop over y-particles within a neighbor
+                        for jj in range(off_y, off_y + y_list.cell_size):
+                            j = y_list.particle_index[jj]
+                            if j == -1:
+                                continue
+                            dist_sq = np.sum((x[:, i] - y[:, j]) ** 2)
+                            if dist_sq == 0:
+                                continue
+                            dist = np.sqrt(dist_sq)
+                            if dist < cutoff:
+                                u[i] += 1 / dist
     return u
 
 
